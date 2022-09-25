1df7d31ea8639249609f36bc0c63a6c6f4410557
Daniil Tiunov <daniil_tiunov@mail.ru>
2022-09-25 20:12:07 +0500

    KEK

--- main.cpp
+++ main.cpp
@@ -3,3 +3,14 @@
 using namespace std;
 
 int main()
+
+{
+    int a;
+    int b;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "a-b = " << (a-b);
+    return 0;
+ }
